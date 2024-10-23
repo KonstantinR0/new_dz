9367d6b0bb1f788bdfc2dac4136f150519e66e33
KonstantinR0 <kostik-rovin@mail.ru>
2024-10-23 13:32:53 +0500

    Chek! new if

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 num = int(input("Введите число от 1 до 5: "))
 numbers = {1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five"}
 
-if num < 6:
+if (num < 6) and (num > 0):
     print(numbers[num])
 else:
     print("Ошибка, до свидания!")
